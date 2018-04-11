03b2a25346006e7d87465db08572cb83e00c5f14
João Delgado <joaomtdelgado@gmail.com>
2018-04-12 00:08:41 +0100

    Change graphics framework from Piston to ggez

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,29 @@
-extern crate glutin_window;
-extern crate graphics;
-extern crate opengl_graphics;
-extern crate piston;
+extern crate ggez;
 extern crate rand;
 
-use piston::window::WindowSettings;
-use piston::event_loop::{EventSettings, Events};
-use opengl_graphics::{GlGraphics, OpenGL};
-use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
-use glutin_window::GlutinWindow as Window;
 use rand::{thread_rng as random, Rng};
 
-use graphics::types::Color;
-use graphics::color::{BLACK, WHITE};
+use ggez::graphics::Color;
+use ggez::graphics::{BLACK, WHITE};
+use ggez::*;
+
+type Point = nalgebra::geometry::Point2<f32>;
 
 const WIDTH: u32 = 1280;
 const HEIGTH: u32 = 720;
 
-const CIRCLES_PER_SECOND: u32 = 100;
+const CIRCLES_PER_SECOND: u32 = 60;
 const SECONDS_PER_CIRCLE: f64 = 1.0 / CIRCLES_PER_SECOND as f64;
 
-const CIRCLE_SPACING: u32 = 30;
-const CIRCLE_BORDER: f64 = 1.0;
-const CIRCLE_EXPANSION_RATE: f64 = 50.0;
-
-#[derive(PartialEq, Clone)]
-struct Point {
-    x: f64,
-    y: f64,
-}
-
-impl Point {
-    fn dst(&self, other: &Point) -> f64 {
-        ((self.x - other.x).powi(2) + (self.y - other.y).powi(2)).sqrt()
-    }
-}
+const CIRCLE_SPACING: u32 = 15;
+const CIRCLE_BORDER: f32 = 1.0;
+const CIRCLE_EXPANSION_RATE: f32 = 75.0;
 
 #[derive(PartialEq, Clone)]
 struct Circle {
     center: Point,
     color: Color,
-    radius: f64,
+    radius: f32,
     expanding: bool,
 }
 
@@ -54,82 +37,99 @@ impl Circle {
         }
     }
 
-    fn render(&mut self, args: &RenderArgs, gl: &mut GlGraphics) {
-        use graphics::ellipse::{circle, Ellipse};
-
-        gl.draw(args.viewport(), |c, gl| {
-            Ellipse::new_border(self.color, CIRCLE_BORDER).draw(
-                circle(self.center.x, self.center.y, self.radius),
-                &c.draw_state,
-                c.transform,
-                gl,
-            );
-        });
+    fn render(&mut self, ctx: &mut Context) -> GameResult<()> {
+        use graphics::DrawMode;
+        graphics::circle(
+            ctx,
+            DrawMode::Line(CIRCLE_BORDER),
+            self.center,
+            self.radius,
+            0.1,
+        )?;
+        Ok(())
     }
 
     fn is_colliding(&self, other: &Circle) -> bool {
+        use nalgebra::distance;
         let minimum_space = self.radius + CIRCLE_BORDER + CIRCLE_BORDER + other.radius;
-        self.center.dst(&other.center) < minimum_space
+        distance(&self.center, &other.center) < minimum_space
     }
 
     #[cfg_attr(rustfmt, rustfmt_skip)]
     fn is_edge_colliding(&mut self) -> bool {
         let space = self.radius + CIRCLE_BORDER;
-        self.center.x + space >= WIDTH as f64
+        self.center.x + space >= WIDTH as f32
             || self.center.x - space <= 0.0
-            || self.center.y + space >= HEIGTH as f64
+            || self.center.y + space >= HEIGTH as f32
             || self.center.y - space <= 0.0
     }
 
-    fn expand(&mut self, args: &UpdateArgs) {
+    fn expand(&mut self, ctx: &Context) {
         if self.expanding {
-            self.radius += CIRCLE_EXPANSION_RATE * args.dt;
+            self.radius +=
+                CIRCLE_EXPANSION_RATE * timer::duration_to_f64(timer::get_delta(ctx)) as f32;
         }
     }
 }
 
-struct App {
+struct MainState {
     circles: Vec<Circle>,
     available_spaces: Vec<Point>,
     dt_to_next_circle: f64,
 }
 
-impl App {
-    fn new() -> App {
+impl MainState {
+    fn new(ctx: &mut Context) -> MainState {
+        graphics::set_background_color(ctx, BLACK);
+
         let mut available_spaces = vec![];
         for x in 0..(WIDTH / CIRCLE_SPACING) {
             for y in 0..(HEIGTH / CIRCLE_SPACING) {
-                available_spaces.push(Point {
-                    x: (x * CIRCLE_SPACING) as f64,
-                    y: (y * CIRCLE_SPACING) as f64,
-                });
+                available_spaces.push(Point::new(
+                    (x * CIRCLE_SPACING) as f32,
+                    (y * CIRCLE_SPACING) as f32,
+                ));
             }
         }
         random().shuffle(&mut available_spaces);
 
-        App {
+        MainState {
             circles: vec![],
             available_spaces: available_spaces,
             dt_to_next_circle: 0.0,
         }
     }
 
-    fn render(&mut self, args: &RenderArgs, gl: &mut GlGraphics) {
-        use graphics::clear;
-
-        clear(BLACK, gl);
-
-        self.circles
-            .iter_mut()
-            .for_each(|circle| circle.render(args, gl));
-    }
-
-    fn update(&mut self, args: &UpdateArgs) {
-        if self.dt_to_next_circle <= 0.0 {
-            self.create_circle();
-            self.dt_to_next_circle = SECONDS_PER_CIRCLE;
+    fn create_circle(&mut self, ctx: &mut Context) {
+        if self.available_spaces.is_empty() {
+            return;
         }
-        self.dt_to_next_circle -= args.dt;
+
+        while timer::check_update_time(ctx, 60) {
+            if let Some(center) = self.available_spaces.pop() {
+                println!("Spaces to fill: {}", self.available_spaces.len());
+                let new = Circle::new_at(center);
+                if self.circles
+                    .iter()
+                    .all(|existing| !new.is_colliding(existing))
+                {
+                    self.circles.push(new);
+                    return;
+                }
+            } else {
+                return;
+            }
+        }
+    }
+}
+
+impl event::EventHandler for MainState {
+    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
+        if self.dt_to_next_circle <= 0.0 {
+            self.create_circle(ctx);
+            self.dt_to_next_circle = SECONDS_PER_CIRCLE as f64;
+        }
+        self.dt_to_next_circle -= timer::duration_to_f64(timer::get_delta(ctx));
 
         let other_circles = self.circles.clone();
         for c1 in self.circles.iter_mut().filter(|c| c.expanding) {
@@ -149,56 +149,34 @@ impl App {
                 }
             }
         }
-
         for circle in self.circles.iter_mut().filter(|c| c.expanding) {
-            circle.expand(args);
+            circle.expand(ctx);
         }
+
+        Ok(())
     }
 
-    fn create_circle(&mut self) {
-        if self.available_spaces.is_empty() {
-            return;
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx);
+
+        for circle in &mut self.circles {
+            circle.render(ctx)?;
         }
 
-        loop {
-            if let Some(center) = self.available_spaces.pop() {
-                let new = Circle::new_at(center);
-                if self.circles
-                    .iter()
-                    .all(|existing| !new.is_colliding(existing))
-                {
-                    self.circles.push(new);
-                    println!("Spaces to fill: {}", self.available_spaces.len());
-                    return;
-                }
-            } else {
-                println!("Done!");
-                return;
-            }
-        }
+        graphics::present(ctx);
+        timer::yield_now();
+        Ok(())
     }
 }
 
 fn main() {
-    let opengl = OpenGL::V3_2;
-
-    let mut window: Window = WindowSettings::new("Space filling circles", [WIDTH, HEIGTH])
-        .opengl(opengl)
-        .exit_on_esc(true)
+    let mut ctx = ContextBuilder::new("Space filling circles", "João Delgado")
+        .window_setup(conf::WindowSetup::default().title("Space filling circles"))
+        .window_mode(conf::WindowMode::default().dimensions(WIDTH, HEIGTH))
         .build()
-        .expect("Error creating window");
-
-    let mut gl = GlGraphics::new(opengl);
-    let mut app = App::new();
-
-    let mut events = Events::new(EventSettings::new());
-    while let Some(e) = events.next(&mut window) {
-        if let Some(r) = e.render_args() {
-            app.render(&r, &mut gl);
-        }
-
-        if let Some(u) = e.update_args() {
-            app.update(&u);
-        }
+        .expect("Error building context");
+    let mut state = MainState::new(&mut ctx);
+    if let Err(e) = event::run(&mut ctx, &mut state) {
+        println!("[ERROR] {}", e);
     }
 }
